a0fc0323e87c6d4ca26dd821b63bc5246102b922
Meenakahiraj <mca528@rajagiri.edu>
2021-05-30 12:34:03 +0530

    bakes

--- database/migrations/2021_05_30_070247_create_cart_table.php
+++ database/migrations/2021_05_30_070247_create_cart_table.php
@@ -15,7 +15,7 @@ class CreateCartTable extends Migration
     {
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->string('user_id');
             $table->integer('product_id');
             $table->timestamps();
         });
